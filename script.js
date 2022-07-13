ef7e6189d3c56d8987765fe47391a07353065b09
Fernanda Maximiano de Souza <104921691+fmakximiano@users.noreply.github.com>
2022-07-13 00:36:04 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+//Criar um array que receba 5 itens e exiba no consolo
+let amigos =["Fernanda","Lucas","João","Elenita","Johnny"] 
+    console.log(amigos)
+
+//2. Utilizar método para adicionar um nome ao inicio do array
+amigos.unshift("Ester")
+
+//3.Utilizar um método para remover o último nome do array
+amigos.shift("Johnny")
+
+//4. Utilizar um método para adicionar dois nomes ao fim do array
+amigos.push("Bruno","Johnny")
+
+//5.Utilizar um método para remover o primeiro nome do array
+amigos.shift("Fernanda")
+
+//6.Utilizar um método para organizar em ordem crescemte o seguinte array: let numbers = [7,5,6,3,8,9,2,1,4]
+let numbers =[7,5,6,3,8,9,2,1,4]
+numbers.sort(){ 
+    console.log(numbers)
+}
